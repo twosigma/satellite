4ce6972020dc5fb90096fb8bfcebfa70402fff0d
Jeff Weiss <me@jweiss.com>
2015-04-22 17:51:20 -0400

    Trying to figure out how to get the leader ip address

--- satellite-master/config/satellite-config.clj
+++ satellite-master/config/satellite-config.clj
@@ -3,7 +3,7 @@
 (def settings
   (merge settings
     {:mesos-master-url
-     (fnk [] (url/url "http://localhost:5050"))
+     (fnk [] (url/url "http://10.165.223.247:5050"))
      :riak (fnk [] nil)
      :sleep-time (fnk [] 5000)
      :zookeeper (fnk [] "10.176.0.79:2181")
