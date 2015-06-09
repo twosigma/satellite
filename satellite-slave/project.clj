118671ea57b880bc0173cbca37683f02ee2d4107
Sunil Abraham <sabraham@twosigma.com>
2015-06-09 11:46:07 -0400

    Update project.cljs

--- satellite-slave/project.clj
+++ satellite-slave/project.clj
@@ -12,7 +12,8 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 
-(defproject satellite-slave "0.1.0-SNAPSHOT"
+(defproject satellite-slave "0.1.0"
+  :description "Monitoring and alerting for Mesos"
   :url "http://github.com/twosigma/satellite/satellite-slave"
   :license {:name "Apache License, Version 2.0"
             :url "http://opensource.org/licenses/apache2.0.php"}
